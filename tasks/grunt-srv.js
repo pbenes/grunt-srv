40bd056ec5342221602bbf74e9c1bd350123dc74
Petr Benes <petr.benes@gooddata.com>
2013-03-23 20:02:23 +0100

    CODE: Task configurable via options

--- tasks/grunt-srv.js
+++ tasks/grunt-srv.js
@@ -1,4 +1,3 @@
-/*global node:true*/
 /*
  * grunt-srv
  *
@@ -6,66 +5,74 @@
  */
 module.exports = function(grunt) {
 
-    var fs = require('fs');
-    var path = require('path');
-
-    var mountFolder = function (connect, dir) {
-        return connect.static(path.resolve(dir));
-    };
-    var proxySnippet = require('grunt-connect-proxy/lib/utils').proxyRequest;
-
-    // relative to the gruntfile which uses grunt-srv
-    var certificatePath = path.resolve('./cert');
-
-    grunt.initConfig({
-        connect: {
-            server: {
-                options: {
-                    protocol: 'https',
-                    port: 8443,
-                    key: fs.readFileSync(certificatePath + '/server.key').toString(),
-                    cert: fs.readFileSync(certificatePath + '/server.crt').toString(),
-                    ca: fs.readFileSync(certificatePath + '/ca.crt').toString(),
-                    passphrase: 'grunt',
-                    keepalive: true,
-                    middleware: function(connect) {
-                        return [
-                            function(req, res, next) {
-                                var writeHead = res.writeHead;
-                                res.writeHead = function(statusCode, headers) {
-                                    // headers['set-cookie'] is an array of set-cookie strings
-                                    if (headers && headers['set-cookie'] && headers['set-cookie'].length) {
-                                        headers['set-cookie'] = headers['set-cookie'].map(function(setCookieHeader) {
-                                            return setCookieHeader.replace(/(domain=[^ ]+; )/mg, '');
-                                        });
-                                    }
-
-                                    res.writeHead = writeHead;
-                                    res.writeHead.apply(this, [statusCode, headers]);
-                                };
-                                next();
-                            },
-                            proxySnippet,
-                            mountFolder(connect, 'html')
-                        ];
-                    }
-                }
-            },
-            proxies: [
-                {
-                    context: '/gdc',
-                    host: 'secure.gooddata.com',
-                    port: 443,
-                    https: true,
-                    changeOrigin: true
-                }
-            ]
-        }
-    });
-
     grunt.loadNpmTasks('grunt-srv/node_modules/grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-srv/node_modules/grunt-connect-proxy');
     grunt.registerTask('grunt-srv', function(target) {
+
+        var fs = require('fs'),
+            path = require('path'),
+            proxySnippet = require('grunt-connect-proxy/lib/utils').proxyRequest;
+
+        var mountFolder = function(connect, dir) {
+            return connect.static(path.resolve(dir));
+        };
+
+        // merge options with defaults
+        var taskOptions = this.options({
+            port: 8443,
+            backendPrefix: '/gdc',
+            backendHost: 'secure.gooddata.com',
+            backendPort: 443
+        });
+
+        // relative to the gruntfile which uses grunt-srv
+        var certificatePath = path.resolve('./cert');
+
+        grunt.initConfig({
+            connect: {
+                server: {
+                    options: {
+                        protocol: 'https',
+                        port: taskOptions.port,
+                        key: fs.readFileSync(certificatePath + '/server.key').toString(),
+                        cert: fs.readFileSync(certificatePath + '/server.crt').toString(),
+                        ca: fs.readFileSync(certificatePath + '/ca.crt').toString(),
+                        passphrase: 'grunt',
+                        keepalive: true,
+                        middleware: function(connect) {
+                            return [
+                                function(req, res, next) {
+                                    var writeHead = res.writeHead;
+                                    res.writeHead = function(statusCode, headers) {
+                                        // headers['set-cookie'] is an array of set-cookie strings
+                                        if (headers && headers['set-cookie'] && headers['set-cookie'].length) {
+                                            headers['set-cookie'] = headers['set-cookie'].map(function(setCookieHeader) {
+                                                return setCookieHeader.replace(/(domain=[^ ]+; )/mg, '');
+                                            });
+                                        }
+
+                                        res.writeHead = writeHead;
+                                        res.writeHead.apply(this, [statusCode, headers]);
+                                    };
+                                    next();
+                                },
+                                proxySnippet,
+                                mountFolder(connect, 'html')
+                            ];
+                        }
+                    }
+                },
+                proxies: [{
+                        context: taskOptions.backendPrefix,
+                        host: taskOptions.backendHost,
+                        port: taskOptions.backendPort,
+                        https: true,
+                        changeOrigin: true
+                }]
+            }
+        });
+
+
         grunt.task.run([
             'configureProxies',
             'connect'
